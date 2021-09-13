f039b5cc1ee7a0d8f820e501c478dae9f1e1fd83
KeonWoo Kim <52156247+kwkim95@users.noreply.github.com>
2021-09-13 23:51:35 +0900

    upload file

--- number.c
+++ number.c
@@ -3,7 +3,8 @@
 #include <time.h>
 #include <psapi.h>
 
-double cputime() { 
+double cputime() 
+{ 
 	FILETIME kernel, user, idle;
 
 	GetSystemTimes(&idle, &kernel, &user);
@@ -30,15 +31,13 @@ double cputime() {
 	double u = (cur_u2 + prev_u2) - (cur_u1 + prev_u1); // user 이용률
 	double i = (cur_i2 + prev_i2) - (cur_i1 + prev_i1); // idle
 	
-	double cpu = (k + u - i) * 100 / (k + u);// CPU 사용률 구하는 공식 = (kernel+user-idle) * 100 / (kernel+user)
-
+	double cpu = (k + u - i) * 100 / (k + u); // CPU 사용률 구하는 공식 = (kernel+user-idle) * 100 / (kernel+user)
 
 	return cpu;
-
 }
 
-int main() {
-
+int main() 
+{
 	int count = 0;
 	SYSTEMTIME lt; // Current Time를 저장하는 변수
 	SYSTEM_INFO Info; // Number of processors를 저장하는 변수
@@ -56,8 +55,8 @@ int main() {
 	printf("Number of CPU's : %d\n", Info.dwNumberOfProcessors); 
 	printf("Index\t\tTime\t\tProcesses\tMemoryLoad\tCPULoad\n");
 
-	while (1) {
-
+	while (1) 
+	{
 		count++;
 		GetLocalTime(&lt);
 		double r = cputime();
@@ -69,14 +68,17 @@ int main() {
 
 		if (count <= 5) // 5초 이하 cpu load
 			printf("\t\t%0.2f\n", r);
-		else if (count > 5 && count <= 10) { //6초이상 10초이하 cpu load
+		else if (count > 5 && count <= 10) // 6초이상 10초이하 cpu load
+		{ 
 			printf("\t\t%0.2f\t[5sec avg : %0.2f]\n", r, (double) (r-4 + r - 3 + r - 2 + r - r) / 5);
 		}
-		else if (count > 10 && count <= 15) { // 11초이상 15초이하 cpu load
+		else if (count > 10 && count <= 15) // 11초이상 15초이하 cpu load
+		{
 			printf("\t\t%0.2f\t[5sec avg : %0.2f] [10sec avg : %0.2f]\n",r, (double)(r - 4 + r - 3 + r - 2 + r - r) / 5,
 				(double)(r - 9 + r - 8 + r - 7 + r - 6 + r -5 + r - 4 + r - 3 + r - 2 + r - r) / 10);
 		}
-		else if (count > 15) { // 16초이상 cpu load
+		else if (count > 15) // 16초이상 cpu load
+		{
 			printf("\t\t%0.2f\t[5sec avg : %0.2f] [10sec avg : %0.2f] [15sec avg : %0.2f]\n", r, (double)(r - 4 + r - 3 + r - 2 + r - r) / 5,
 				(double)(r - 9 + r - 8 + r - 7 + r - 6 + r - 5 + r - 4 + r - 3 + r - 2 + r - r) / 10, 
 				(double)(r - 14 + r - 13 + r - 12 + r - 11 + r - 10 + r - 9 + r - 8 + r - 7 + r - 6 + r - 5 + r - 4 + r - 3 + r - 2 + r - r) / 15);
